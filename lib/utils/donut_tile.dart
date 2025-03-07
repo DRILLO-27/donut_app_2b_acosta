bab6b81d419edc93771c59911a229bb398eb745f
DRILLO-27 <drilos482@gmail.com>
2025-03-07 11:29:18 -0600

    DonutTile Finished

--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -6,8 +6,10 @@ class DonutTile extends StatelessWidget {
   //Tipo de color  
   final dynamic donutColor;
   final String imageName;
+  
+  final String donutMark;
 
-  const DonutTile({super.key, required this.donutFlavor, required this.donutPrice, required this.donutColor, required this.imageName});
+  const DonutTile({super.key, required this.donutFlavor, required this.donutMark, required this.donutPrice, required this.donutColor, required this.imageName});
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +37,7 @@ class DonutTile extends StatelessWidget {
                       const EdgeInsets.symmetric(vertical: 8, horizontal:  18),
                   child: Text('\$$donutPrice', 
                   style: TextStyle(fontWeight: FontWeight.bold,
-                  fontSize: 18,
+                  fontSize: 14,
                   color: donutColor[800])),
               )
             ],
@@ -43,11 +45,41 @@ class DonutTile extends StatelessWidget {
           //Donut Picture
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 34),
-            child: Image.asset(imageName))
+            child: Image.asset(imageName)),
           //Donut Text
-
+          Text(donutFlavor,
+              style: 
+                const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+              //Texto con espacio
+              const SizedBox(
+                height: 4,
+              ),
+              //Agregar todo el Texto
+          Text(donutMark,
+              style: 
+                const TextStyle(fontWeight: FontWeight.normal, fontSize: 10),),
+              //Texto con espacio
+              const SizedBox(
+                height: 4,
+              ),
           //Love icon + add button
-        ],),
+              Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16), // Espaciado lateral
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, // Elementos en las esquinas
+                children: [
+                  Icon(Icons.favorite_border, color: Colors.grey.shade700), // Corazón a la izquierda
+                  const Text(
+                    'Add',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, decoration: TextDecoration.underline),
+                  ), // "Add" a la derecha
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 12), // Espaciado final
+          ],
+        ),
       ),
     );
   }
