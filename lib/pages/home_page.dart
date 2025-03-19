8aa311b5306aa67eac4568f3b2feac5fc0a301f1
DRILLO-27 <drilos482@gmail.com>
2025-03-19 11:02:26 -0600

    UI finished

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,70 +14,111 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //Lista de Tabs
   List<Widget> myTabs = [
-    //Donut Tab
-    MyTab(iconPath: 'lib/icons/donut.png'),
-    //Burger Tab
-    MyTab(iconPath: 'lib/icons/burger.png'),
-    //Smoothie Tab
-    MyTab(iconPath: 'lib/icons/smoothie.png'),
-    //Pancake Tab
-    MyTab(iconPath: 'lib/icons/pancakes.png'),
-    //Pizza Tab
-    MyTab(iconPath: 'lib/icons/pizza.png')
+    //Donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    //Burger tab
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+    //Smoothie tab
+    const MyTab(iconPath: 'lib/icons/smoothie.png'),
+    //Pancake tab
+    const MyTab(iconPath: 'lib/icons/pancakes.png'),
+    //Pizza tab
+    const MyTab(iconPath: 'lib/icons/pizza.png'),
   ];
+
   @override
   Widget build(BuildContext context) {
+    //Este widget sirve para gestionar las pestañas
     return DefaultTabController(
-      length:  5,
+      length: myTabs.length,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          //Icono Izquierdo
-          leading: Icon(Icons.menu, 
-          color: Colors.greenAccent[800],),
-          //Iconone Derecho
-          actions: const[
-            Padding(
-              padding: const EdgeInsets.only(right: 24.0),
-              child: Icon(Icons.person),
-            )
-          ],
-        ),
-        body: Column(children: [
-          //Texto principal
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
-            child: Row(
-              children: [
-                Text("I want to ", style: TextStyle(fontSize: 32)),
-                Text("Eat", style: TextStyle(
-                  //Tamaño de Letra
-                  fontSize: 32, 
-                  //Negrita
-                  fontWeight: FontWeight.bold, 
-                  //Subrayado
-                  decoration: TextDecoration.underline))
-              ],
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            //Icono de la izquierda
+            leading: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
             ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 24.0),
+                child: Icon(Icons.person),
+              )
+            ],
           ),
-          //TabBar
-          TabBar(tabs: myTabs),
-          //TabBarView
-          Expanded(
-            child: TabBarView(children: [
-              DonutTab(),
-              BurgerTab(),
-              SmoothieTab(),
-              PancakesTab(),
-              PizzaTab()
-            ]),
-          ),
-
-          //Carrito
-        ],)
-      ),
+          body: Column(
+            children: [
+              //Texto "I want to eat"
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+                child: Row(
+                  children: [
+                    Text(
+                      "I want to ",
+                      style: TextStyle(fontSize: 32),
+                    ),
+                    Text("Eat",
+                        style: TextStyle(
+                            //Tamaño de letra
+                            fontSize: 32,
+                            //Negritas
+                            fontWeight: FontWeight.bold,
+                            //Subrayado
+                            decoration: TextDecoration.underline))
+                  ],
+                ),
+              ),
+              //Tab Bar (Pestañas)
+              TabBar(tabs: myTabs),
+              //Tab bar View (Contenido de pestañas)
+              Expanded(
+                  child: TabBarView(children: [
+                DonutTab(),
+                BurgerTab(),
+                SmoothieTab(),
+                PancakesTab(),
+                PizzaTab(),
+              ])),
+              //Carrito
+              Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(left: 28),
+                          child: Column(
+                            //Alinear a la izquierda (horizontal)
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('2 Items | \$45',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold)),
+                              Text(
+                                'Delivery Charges Included',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          )),
+                      ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 218, 113, 148),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 12)),
+                          child: const Text(
+                            'View Cart',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ],
+                  ))
+            ],
+          )),
     );
   }
 }
