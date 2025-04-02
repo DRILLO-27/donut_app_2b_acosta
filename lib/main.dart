e473e1ddb1604bf526bacf7a5466bf3aa01e8f0b
Pedrodiaxx <joel.diaz.lopez7@gmnail.com>
2025-04-01 22:21:15 -0600

    Connecting to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:donut_app_2b_acosta/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:donut_app_2b_acosta/pages/login_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
